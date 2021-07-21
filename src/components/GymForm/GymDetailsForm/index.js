80abf9f2d8608ae6083048638584c3faace134c1
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-07-21 19:55:27 +0100

    added css file for gym details form

--- src/components/GymForm/GymDetailsForm/index.js
+++ src/components/GymForm/GymDetailsForm/index.js
@@ -2,9 +2,7 @@ const GymDetailsForm = (props) => {
   const { name, address, postCode, city, contactNumber } = props;
   return (
     <div>
-      <form>
-        <h1>{name}</h1>
-      </form>
+      <form></form>
     </div>
   );
 };
