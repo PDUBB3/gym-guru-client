b2ea01966d8ccf92a51573af5484dc40ff6a6624
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-07-21 22:08:37 +0100

    other facilities checkboxes wip

--- src/components/GymForm/OtherFacilitiesForm/index.js
+++ src/components/GymForm/OtherFacilitiesForm/index.js
@@ -1,10 +1,20 @@
-// import { ToggleButtonCheckbox } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const OtherFacilitiesForm = (props) => {
   const { otherFacilities } = props;
-  return <div id="container">{otherFacilities}</div>;
+  return (
+    <div id="container">
+      {["checkbox", "radio"].map((otherFacilities) => {
+        return <div key={otherFacilities} className="mb-3"></div>;
+      })}
+
+      {/* // .map((otherFacilities) => (
+      //   <div key={`default-${otherFacilities}`} className="mb-3"></div>
+      // ))} */}
+    </div>
+  );
 };
 
 export default OtherFacilitiesForm;
