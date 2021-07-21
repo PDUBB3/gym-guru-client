4492619d549f907ae28f85481df16e5e96a30ca5
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-07-21 21:47:55 +0100

    changed file names to keep naming conventions

---
+++ src/components/GymForm/OtherFacilitiesForm/index.js
@@ -0,0 +1,10 @@
+// import { ToggleButtonCheckbox } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const OtherFacilitiesForm = (props) => {
+  const { otherFacilities } = props;
+  return <div id="container">{otherFacilities}</div>;
+};
+
+export default OtherFacilitiesForm;
