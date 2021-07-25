792b615f5b2c4e57fd4fa8e0c5e9d2432f7b8f13
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-07-25 15:43:34 +0100

    add state for multiform

--- src/components/GymForm/GymDetailsForm/index.js
+++ src/components/GymForm/GymDetailsForm/index.js
@@ -1,15 +1,9 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Form, FormField, Box, Button, TextInput } from "grommet";
-import styled from "styled-components";
 
-import "./GymDetailsForm.css";
 import FormInput from "../../FormInput";
 
-const MyStyledButton = styled(Button)`
-  font-weight: bold;
-  background-color: #743ad5;
-`;
+import "./GymDetailsForm.css";
 
 const CreateGymDetailsForm = () => {
   const {
@@ -18,40 +12,82 @@ const CreateGymDetailsForm = () => {
     formState: { errors },
   } = useForm();
 
+  const [formNumber, setFormNumber] = useState(1);
+
   const onSubmit = (formData) => {
     console.log(formData);
   };
 
+  const onClickPrevious = () => {
+    setFormNumber(formNumber - 1);
+  };
+
+  const onClickNext = () => {
+    setFormNumber(formNumber + 1);
+  };
+
+  const renderFormOne = () => {
+    return (
+      <div>
+        <FormInput
+          placeholder="Name"
+          error={errors.name}
+          register={register("name", { required: true })}
+        />
+        <FormInput
+          placeholder="Address"
+          error={errors.address}
+          register={register("address", { required: true })}
+        />
+        <FormInput
+          placeholder="City"
+          error={errors.city}
+          register={register("city", { required: true })}
+        />
+        <FormInput
+          placeholder="Postcode"
+          error={errors.postcode}
+          register={register("postcode", { required: true })}
+        />
+        <FormInput
+          placeholder="Contact Number"
+          error={errors.contactNumber}
+          register={register("contactNumber", { required: true })}
+        />
+      </div>
+    );
+  };
+
+  const renderForm = () => {
+    if (formNumber === 1) {
+      return renderFormOne();
+    }
+    if (formNumber === 2) {
+      return <div>Form 2</div>;
+    }
+    if (formNumber === 3) {
+      return <div>Form 3</div>;
+    }
+    if (formNumber === 4) {
+      return <div>Form 4</div>;
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormInput
-        placeholder="Name"
-        error={errors.name}
-        register={register("name", { required: true })}
-      />
-      <FormInput
-        placeholder="Address"
-        error={errors.address}
-        register={register("address", { required: true })}
-      />
-      <FormInput
-        placeholder="City"
-        error={errors.city}
-        register={register("city", { required: true })}
-      />
-      <FormInput
-        placeholder="Postcode"
-        error={errors.postcode}
-        register={register("postcode", { required: true })}
-      />
-      <FormInput
-        placeholder="Contact Number"
-        error={errors.contactNumber}
-        register={register("contactNumber", { required: true })}
-      />
-
+      {renderForm()}
       <div className="button-block">
-        <button type="submit">Submit</button>
+        {formNumber !== 1 && (
+          <button type="button" onClick={onClickPrevious}>
+            Previous
+          </button>
+        )}
+        {formNumber !== 4 && (
+          <button type="button" onClick={onClickNext}>
+            Next
+          </button>
+        )}
+        {formNumber === 4 && <button type="submit">Submit</button>}
       </div>
     </form>
   );
