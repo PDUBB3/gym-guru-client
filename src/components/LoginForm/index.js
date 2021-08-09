76379a0cb5cd5303c30bc5fcf5eeedf797ff447e
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-08-09 21:39:12 +0100

    login form complete

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,26 +1,38 @@
-import FormInput from "../../FormInput";
+import { useForm } from "react-hook-form";
+import FormInput from "../FormInput";
 
 import "./LoginForm.css";
 
-const LoginForm = ({ errors, register }) => {
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
   return (
     <div class="form-box">
-      <div>
-        <h2 class="form-heading">Sign in</h2>
-      </div>
-      <FormInput
-        placeholder="Username"
-        error={errors.username}
-        register={register("username", { required: true })}
-      />
-      <FormInput
-        placeholder="Password"
-        error={errors.password}
-        register={register("password", { required: true })}
-      />
-      <button className="button border-gradient" type="button">
-        Sign in
-      </button>
+      <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <h2 class="form-heading">Sign in</h2>
+        </div>
+        <FormInput
+          placeholder="Username"
+          error={errors.username}
+          register={register("username", { required: true })}
+        />
+        <FormInput
+          placeholder="Password"
+          error={errors.password}
+          register={register("password", { required: true })}
+        />
+
+        <button className="button border-gradient" type="submit">
+          Sign in
+        </button>
+      </form>
     </div>
   );
 };
