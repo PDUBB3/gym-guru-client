556740bac16e530ff39a68a65e04bf8f7b01f0e2
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-08-10 19:32:49 +0100

    signup form wip

---
+++ src/components/SignUpForm/index.js
@@ -0,0 +1,65 @@
+import { useForm } from "react-hook-form";
+import FormInput from "../FormInput";
+
+import "./SignUpForm.css";
+
+const SignUpForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <div class="form-box">
+      <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <h2 class="form-heading">Sign Up Here</h2>
+        </div>
+        <FormInput
+          placeholder="First name"
+          error={errors.firstName}
+          register={register("firstName", { required: true })}
+        />
+        <FormInput
+          placeholder="Last name"
+          error={errors.lastName}
+          register={register("lastName", { required: true })}
+        />
+        <FormInput
+          placeholder="Email"
+          error={errors.email}
+          register={register("email", { required: true })}
+        />
+        <FormInput
+          placeholder="Password"
+          error={errors.password}
+          register={register("password", { required: true })}
+        />
+        <FormInput
+          placeholder="Username"
+          error={errors.username}
+          register={register("username", { required: true })}
+        />
+        <FormInput
+          placeholder="Username"
+          error={errors.username}
+          register={register("username", { required: true })}
+        />
+        <FormInput
+          placeholder="Password"
+          error={errors.password}
+          register={register("password", { required: true })}
+        />
+
+        <button className="button border-gradient" type="submit">
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,7 +1,10 @@
+import SignUpForm from "../components/SignUpForm";
+
 const SignupPage = () => {
   return (
     <div>
       <h1>This is the signup page</h1>
+      <SignUpForm />
     </div>
   );
 };
