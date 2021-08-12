a15f3467d354c2716bd42768b43363ac29fa66cc
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-08-12 19:13:01 +0100

    login form hooked to mutation

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,5 +1,4 @@
 import { useForm } from "react-hook-form";
-
 import { useContext } from "react";
 import { useMutation } from "@apollo/client";
 import { useHistory } from "react-router-dom";
@@ -7,19 +6,52 @@ import { useHistory } from "react-router-dom";
 import FormInput from "../FormInput";
 import PasswordInput from "../PasswordInput";
 import { UserContext } from "../../context/UserContext";
-import LOGIN from "../../graphql/mutations";
+import { LOGIN } from "../../graphql/mutations";
 
 import "./LoginForm.css";
 
-const LoginForm = () => {
+const LoginForm = ({ redirect }) => {
+  const history = useHistory();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (formData) => {
-    console.log(formData);
+
+  const [login, { data, error, loading }] = useMutation(LOGIN, {
+    onCompleted: (data) => {
+      const {
+        token,
+        user: { email, id, firstName, lastName },
+      } = data.login;
+
+      onLogin({
+        id,
+        email,
+        token,
+        firstName,
+        lastName,
+      });
+
+      history.push(redirect || "/");
+    },
+    onError: () => {},
+  });
+
+  const { onLogin } = useContext(UserContext);
+
+  const onSubmit = async (formData) => {
+    await login({
+      variables: {
+        loginInput: formData,
+      },
+    });
   };
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div class="form-box">
       <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
