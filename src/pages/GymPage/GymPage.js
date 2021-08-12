eb04aea86f71ad55c0b4347d22e44dad286caa4f
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-08-12 20:33:46 +0100

    added basic layout and info - added rating stars

---
+++ src/pages/GymPage/GymPage.js
@@ -0,0 +1,86 @@
+import "./GymPage.css";
+
+import { FaStar, FaRegStar } from "react-icons/fa";
+
+const GymPage = ({ rating }) => {
+  return (
+    <div className="main-container">
+      <div className="image-container">
+        <img
+          src="https://prod-we-cdn-media.puregym.com/media/805029/calcot-free-weights.jpg?quality=80"
+          alt="pure gym"
+          height="350"
+        />
+      </div>
+      <div className="about-container">
+        <h1 className="title">Pure Gym Solihull</h1>
+        <div className="info-container">
+          {(rating === 10 && (
+            <div>
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+            </div>
+          )) ||
+            (rating >= 8 && (
+              <div>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaRegStar />
+              </div>
+            )) ||
+            (rating >= 6 && (
+              <div>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+            )) ||
+            (rating >= 3 && (
+              <div>
+                <FaStar />
+                <FaStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+            )) ||
+            (rating >= 1 && (
+              <div>
+                <FaStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+            )) ||
+            (rating === 0 && (
+              <div>
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+                <FaRegStar />
+              </div>
+            ))}
+
+          <div>Address:</div>
+          <div>Contact Number:</div>
+          <div>Opening Times:</div>
+          <div>Exercise Facilities:</div>
+          <div>Other Facilities:</div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GymPage;
