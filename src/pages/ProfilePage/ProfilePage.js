00d54fe0f4aa0c5c37d5da470c8bb0075587c034
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-08-14 21:28:11 +0100

    completed about section

---
+++ src/pages/ProfilePage/ProfilePage.js
@@ -0,0 +1,13 @@
+import ProfilePageContent from "../../components/ProfilePage/ProfilePageContent";
+
+import "./ProfilePage.css";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <ProfilePageContent />
+    </div>
+  );
+};
+
+export default ProfilePage;
