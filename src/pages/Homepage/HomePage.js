06145f7bc6456aaa810ccda1356ddaacdd8fca69
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-08-14 10:44:18 +0100

    carousel in

---
+++ src/pages/Homepage/HomePage.js
@@ -0,0 +1,31 @@
+// import { Carousel } from "bootstrap";
+import Carousel from "../../utils/Carousel";
+
+import Header from "../../components/Header/Header";
+
+import "./Homepage.css";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <Header />
+      <Carousel>
+        {
+          <div id="app">
+            <a
+              href="https://youtu.be/5ptXXNjuUfg"
+              target="_blank"
+              rel="noreferrer"
+              data-keyframers-credit
+              style="color: #FFF"
+            >
+              Link text
+            </a>
+          </div>
+        }
+      </Carousel>
+    </>
+  );
+};
+
+export default HomePage;
