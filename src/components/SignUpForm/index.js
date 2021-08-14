e4259075172f42d7fb0fc14f9b5184d109b5d961
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-08-14 13:55:44 +0100

    removed commented out code

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -88,64 +88,6 @@ const SignUpForm = ({ redirect = "/login" }) => {
       {error && !data && <div>Failed to sign up. Please try again.</div>}
     </form>
   );
-
-  // return (
-  //   <div class="signUp-form-box">
-  //     <form className="signUpForm" onSubmit={handleSubmit(onSubmit)}>
-  //       <div>
-  //         <h2 class="form-heading">Sign Up Here</h2>
-  //       </div>
-  //       <FormInput
-  //         placeholder="First name"
-  //         error={errors.firstName}
-  //         register={register("firstName", { required: true })}
-  //       />
-  //       <FormInput
-  //         placeholder="Last name"
-  //         error={errors.lastName}
-  //         register={register("lastName", { required: true })}
-  //       />
-  //       <FormInput
-  //         placeholder="Email"
-  //         error={errors.email}
-  //         register={register("email", { required: true })}
-  //       />
-  //       <FormInput
-  //         placeholder="Username"
-  //         error={errors.username}
-  //         register={register("username", { required: true })}
-  //       />
-
-  //       <PasswordInput register={register} />
-
-  //       <ImageUpload setValue={setValue} />
-
-  //       <FormInput
-  //         placeholder="City"
-  //         error={errors.city}
-  //         register={register("city", { required: true })}
-  //       />
-
-  //       <FormInput
-  //         placeholder="Type a short bio about yourself here"
-  //         error={errors.bio}
-  //         register={register("bio", { required: true })}
-  //       />
-
-  //       <GymOwnerCheckBox register={register} />
-
-  //       <button
-  //         onSubmit={handleSubmit(onSubmit)}
-  //         className="signUpBtn"
-  //         type="submit"
-  //       >
-  //         Sign Up
-  //       </button>
-
-  //       {error && !data && <div>Failed to sign up. Please try again.</div>}
-  //     </form>
-  //   </div>
-  // );
 };
 
 export default SignUpForm;
