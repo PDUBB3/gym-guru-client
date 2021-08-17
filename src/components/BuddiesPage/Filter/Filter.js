ebc355f4a3af19128f68d57a28642763cc7820e7
Leon <leonwheeler08@gmail.com>
2021-08-17 20:00:33 +0100

    Retrieving Gyms and cities from DB for filter.

--- src/components/BuddiesPage/Filter/Filter.js
+++ src/components/BuddiesPage/Filter/Filter.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useContext } from "react";
+import { useQuery } from "@apollo/client";
 
+import { GYMS_QUERY, USERS_QUERY } from "../../../graphql/queries";
 import { BuddiesFilterContext } from "../../../context/BuddiesFilterContext";
 
 import Button from "@material-ui/core/Button";
@@ -9,13 +11,19 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Box from "@material-ui/core/Box";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
-import { listCities } from "cclist";
 
 import "./Filter.css";
 
 const Filter = ({ filterStatus, setFilterStatus }) => {
   // const [countries] = useState(Country.getAllCountries());
   const [selectedGym, setSelectedGym] = useState("");
+
+  const { data, loading, error } = useQuery(GYMS_QUERY);
+  const {
+    data: userData,
+    loading: userLoading,
+    error: userError,
+  } = useQuery(USERS_QUERY);
 
   const { filterParams, setFilterParams } = useContext(BuddiesFilterContext);
 
@@ -36,8 +44,17 @@ const Filter = ({ filterStatus, setFilterStatus }) => {
     setFilterParams({ ...filterParams, gym: event.target.value });
   };
 
-  const cities = listCities("GB").sort();
-  console.log(cities);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error</h1>;
+  }
+
+  const filteredGyms = data.gyms.filter((gym) => {
+    return gym.city === filterParams.city;
+  });
 
   return (
     <div className={`filter ${filterStatus ? "active-filter" : ""}`}>
@@ -48,19 +65,17 @@ const Filter = ({ filterStatus, setFilterStatus }) => {
             <FormControl style={{ minWidth: "200px" }}>
               <InputLabel>City</InputLabel>
               <Select value={filterParams} onChange={handleChangeCity}>
-                {cities.map((city) => {
+                {userData.users.map((user) => {
                   return (
-                    <MenuItem name={city} value={city} key={city}>
-                      {city}
+                    <MenuItem
+                      name={user.city}
+                      value={user.city}
+                      key={user.city}
+                    >
+                      {user.city}
                     </MenuItem>
                   );
                 })}
-                <MenuItem name="Birmingham" value="Birmingham" key="Birmingham">
-                  Birmingham
-                </MenuItem>
-                <MenuItem name="Manchester" value="Manchester" key="Manchester">
-                  Manchester
-                </MenuItem>
               </Select>
             </FormControl>
           </Box>
@@ -69,9 +84,13 @@ const Filter = ({ filterStatus, setFilterStatus }) => {
               <FormControl style={{ minWidth: "200px" }}>
                 <InputLabel>Gym</InputLabel>
                 <Select value={selectedGym} onChange={handleChangeGym}>
-                  <MenuItem value="PureGym" key="PureGym">
-                    PureGym
-                  </MenuItem>
+                  {filteredGyms.map((gym) => {
+                    return (
+                      <MenuItem name={gym.name} value={gym.name} key={gym.name}>
+                        {gym.name}
+                      </MenuItem>
+                    );
+                  })}
                 </Select>
               </FormControl>
             </Box>
