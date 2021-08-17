be0584d7bc3d531ddff6f857482db82420cb6984
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-08-17 15:53:43 +0100

    added background colour and rearranged goals and interests

--- src/components/ProfilePage/AboutSection/index.js
+++ src/components/ProfilePage/AboutSection/index.js
@@ -3,16 +3,18 @@ import { FaEnvelope, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 const AboutSection = ({ firstName, lastName, city, bio, profileImageUrl }) => {
   return (
     <div className="user-info-container">
-      <img
-        src={profileImageUrl}
-        alt={firstName}
-        height="200"
-        width="200"
-        className="profile-image"
-      />
-      <h1>
-        {firstName} {lastName}
-      </h1>
+      <div className="background">
+        <img
+          src={profileImageUrl}
+          alt={firstName}
+          height="200"
+          width="200"
+          className="profile-image"
+        />
+        <h1>
+          {firstName} {lastName}
+        </h1>
+      </div>
       <div className="bio">
         <div className="city">{city}</div>
         <div>{bio}</div>
--- src/components/ProfilePage/InfoSection/index.js
+++ src/components/ProfilePage/InfoSection/index.js
@@ -8,38 +8,47 @@ const InfoSection = ({ user }) => {
   const { interests, goals, buddies } = user;
   return (
     <div className="extra-info-container">
-      <div className="box">
-        <h2>Interests</h2>
-        <div className="cards">
-          {interests.map((interest) => (
-            <div>
+      <div className="goals-interests-container">
+        <div className="box goals-interests">
+          <h2>Interests</h2>
+          <div className="cards">
+            {interests.map((interest) => (
               <div>
-                <GrYoga />
+                <div>
+                  <GrYoga />
+                </div>
+                <div>{interest}</div>
               </div>
-              <div>{interest}</div>
-            </div>
-          ))}
+            ))}
+          </div>
+        </div>
+        <div className="box goals-interests">
+          <h2>Goals</h2>
+          <div className="cards">
+            {goals.map((goal) => (
+              <div>
+                <div>
+                  <FaWeight />
+                </div>
+                <div>{goal}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="box">
-        <h2>Goals</h2>
-        <div className="cards">
-          {goals.map((goal) => (
-            <div>
-              <div>
-                <FaWeight />
-              </div>
-              <div>{goal}</div>
-            </div>
-          ))}
+      <div className="gym-details-container">
+        <div className="box">
+          <h2>Attending Gym</h2>
         </div>
       </div>
-      <div className="box">
-        <h2>Buddies</h2>
-        <div className="cards">
-          {buddies.map((buddy) => (
-            <BuddyCard buddy={buddy} />
-          ))}
+      <div className="buddies-container">
+        <div className="box">
+          <h2>Buddies</h2>
+          <div className="cards">
+            {buddies.map((buddy) => (
+              <BuddyCard buddy={buddy} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
