7eb6ec2d74ec67d4ed25da8d597016a17c9567c1
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-08-19 19:56:46 +0100

    added reviews to gym page

--- src/components/GymPage/GymPageContent/GymPageContent.js
+++ src/components/GymPage/GymPageContent/GymPageContent.js
@@ -1,8 +1,12 @@
+import StarRatings from "react-star-ratings";
+
 import CustomizedAccordions from "../Accordian/Accordian";
 import Rating from "../Rating/Rating";
 
-const GymPageContent = ({ gym }) => {
+const GymPageContent = ({ gym, reviews }) => {
   const { name, rating, imageURL, ...rest } = gym;
+
+  console.log(reviews);
 
   return (
     <div className="gym-container">
@@ -12,13 +16,54 @@ const GymPageContent = ({ gym }) => {
       <div className="about-container">
         <h1 className="title">{name}</h1>
         <div className="info-container">
-          <Rating rating={rating} />
+          <StarRatings
+            rating={rating}
+            numberOfStars={5}
+            starRatedColor="black"
+            starDimension="20px"
+            starSpacing="3px"
+          />
           <div className="accordian">
             <CustomizedAccordions gym={rest} />
           </div>
         </div>
-        <div className="review-container">
-          <div>Reviews</div>
+      </div>
+      <div className="review-container">
+        {/* <h1>Reviews</h1> */}
+        <div>
+          <div className="rating">{rating}</div>
+          <StarRatings
+            rating={rating}
+            numberOfStars={5}
+            starRatedColor="black"
+            starDimension="30px"
+            starSpacing="3px"
+          />
+        </div>
+        <div>
+          <div>
+            {reviews.map((review) => {
+              return (
+                <div className="ratings-breakdown">
+                  {review.categories.map((category) => {
+                    return (
+                      <div className="review">
+                        <span>{category.category}</span>
+                        <StarRatings
+                          rating={category.rating}
+                          numberOfStars={5}
+                          starRatedColor="black"
+                          starDimension="20px"
+                          starSpacing="3px"
+                        />
+                      </div>
+                    );
+                  })}
+                  <div className="comment">{review.comment}</div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
