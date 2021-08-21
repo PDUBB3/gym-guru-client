58e445b96bfbc6628694dc4553f3ae921952fa1e
Pamela <pamela_bhopal@hotmail.co.uk>
2021-08-21 12:01:28 +0100

    goals and interests filter added

---
+++ src/components/FormFilter/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+export default function CheckboxesTags({ options, placeholder, id }) {
+  return (
+    <Autocomplete
+      multiple
+      id={id}
+      options={options}
+      disableCloseOnSelect
+      getOptionLabel={(option) => option.title}
+      renderOption={(option, { selected }) => (
+        <React.Fragment>
+          <Checkbox
+            icon={icon}
+            checkedIcon={checkedIcon}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option.title}
+        </React.Fragment>
+      )}
+      style={{ width: 500 }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="outlined"
+          label={placeholder}
+          placeholder={placeholder}
+        />
+      )}
+    />
+  );
+}
+
+const goals = [
+  { title: "Lose Weight" },
+  { title: "Gain Muscle" },
+  { title: "Toning" },
+  { title: "Make Friends" },
+  { title: "Get Fit" },
+  { title: "Improving Endurance" },
+  { title: "Improve Strength" },
+];
+
+const interests = [
+  { title: "Yoga" },
+  { title: "Cycling" },
+  { title: "Cardio" },
+  { title: "Weight Lifting" },
+  { title: "Endurance" },
+  { title: "Gym Classes" },
+];
--- src/components/SignUpForm/PartTwo.js
+++ src/components/SignUpForm/PartTwo.js
@@ -4,8 +4,26 @@ import ImageUpload from "../ImageUpload";
 
 import "./SignUpForm.css";
 import "../Button/button.css";
+import CheckboxesTags from "../FormFilter/index";
 
 const PartTwo = ({ errors, register, setValue }) => {
+  const goals = [
+    { title: "Lose Weight" },
+    { title: "Gain Muscle" },
+    { title: "Toning" },
+    { title: "Make Friends" },
+    { title: "Get Fit" },
+    { title: "Improving Endurance" },
+    { title: "Improve Strength" },
+  ];
+  const interests = [
+    { title: "Yoga" },
+    { title: "Cycling" },
+    { title: "Cardio" },
+    { title: "Weight Lifting" },
+    { title: "Endurance" },
+    { title: "Gym Classes" },
+  ];
   return (
     <div className="signUp-form-box">
       <ImageUpload setValue={setValue} />
@@ -18,6 +36,12 @@ const PartTwo = ({ errors, register, setValue }) => {
         placeholder="Type a short bio about yourself here"
         error={errors.bio}
         register={register("bio", { required: true })}
+      />
+      <CheckboxesTags options={goals} placeholder="Goals" id="goals" />
+      <CheckboxesTags
+        options={interests}
+        placeholder="Interests"
+        id="interests"
       />
       <GymOwnerCheckBox register={register} />
     </div>
