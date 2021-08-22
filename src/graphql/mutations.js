e951b8ea438a91ea96281bde00b6545fd42c81cf
Leon <leonwheeler08@gmail.com>
2021-08-22 22:02:38 +0100

    Resolved subfield issue

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -58,8 +58,12 @@ export const SIGNUP = gql`
 export const BUDDYREQUESTS = gql`
   mutation Mutation($buddyRequestsInput: BuddyInput) {
     buddyRequests(input: $buddyRequestsInput) {
-      requesterId
-      recipientId
+      requesterId {
+        username
+      }
+      recipientId {
+        username
+      }
       status
     }
   }
@@ -82,8 +86,12 @@ export const ACCEPTBUDDYREQUEST = gql`
 export const REJECTBUDDYREQUEST = gql`
   mutation Mutation($rejectBuddyRequestInput: BuddyInput) {
     rejectBuddyRequest(input: $rejectBuddyRequestInput) {
-      requesterId
-      recipientId
+      requesterId {
+        username
+      }
+      recipientId {
+        username
+      }
       status
     }
   }
