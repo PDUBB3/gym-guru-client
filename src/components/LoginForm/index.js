3e5926b5d026104ca43347ae76afdb9db1e62659
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-08-23 19:27:19 +0100

    changed form to controller and move form input into component

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,12 +1,18 @@
 import { useForm } from "react-hook-form";
-import { useContext } from "react";
+import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { useHistory } from "react-router-dom";
 
-import FormInput from "../FormInput";
-import PasswordInput from "../PasswordInput";
+import { Controller } from "react-hook-form";
+import Box from "@material-ui/core/Box";
+import classNames from "classnames";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+
 import { useUserContext } from "../../context/UserContext";
 import { LOGIN } from "../../graphql/mutations";
+import FormInput from "../FormInput";
 
 import "./LoginForm.css";
 import "../Button/button.css";
@@ -14,11 +20,12 @@ import "../Button/button.css";
 const LoginForm = ({ redirect }) => {
   const history = useHistory();
   const { dispatch } = useUserContext();
+  const [passwordShown, setPasswordShown] = useState(false);
 
   const {
-    register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm();
 
   const [login, { data, error, loading }] = useMutation(LOGIN, {
@@ -54,18 +61,40 @@ const LoginForm = ({ redirect }) => {
   if (loading) {
     return <h1>Loading...</h1>;
   }
+
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+
   return (
     <div className="form-box">
       <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <h2 className="form-heading">Sign in</h2>
         </div>
-        <FormInput
-          placeholder="Username"
-          error={errors.username}
-          register={register("username", { required: true })}
-        />
-        <PasswordInput register={register("password", { required: true })} />
+        <FormInput control={control} placeholder="Username" name="username" />
+        <Box component="div" m={1}>
+          <Controller
+            name="password"
+            control={control}
+            rules={{ required: "Password is required" }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <FormControl>
+                <InputLabel className={classNames({ "form-error": error })}>
+                  Password
+                </InputLabel>
+                <Input
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  label="Password"
+                  type={passwordShown ? "text" : "password"}
+                />
+              </FormControl>
+            )}
+          />
+        </Box>
+        <button onClick={togglePassword}>Show</button>
 
         <button className="button hover" type="submit">
           <span>Sign in</span>
