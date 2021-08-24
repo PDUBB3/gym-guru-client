c906e8bbb4da12422be2c9f4aded126ba6ad2869
surajverma2587 <surajverma2587@gmail.com>
2021-08-24 17:50:28 +0100

    complete filter

--- src/components/GymPage/GymFilter/index.js
+++ src/components/GymPage/GymFilter/index.js
@@ -1,33 +1,106 @@
 import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { useMediaQuery } from "react-responsive";
+import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import FormGroup from "@material-ui/core/FormGroup";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Box from "@material-ui/core/Box";
+import Divider from "@material-ui/core/Divider";
+import CloseIcon from "@material-ui/icons/Close";
+
+import { DESKTOP_BREAKPOINT } from "../../../mediaQueries";
 
 const exerciseFacilities = [
   {
-    id: "1",
+    id: "612405ab0df2da8865e56d92",
     name: "Weight Area",
   },
   {
-    id: "2",
+    id: "612405ab0df2da8865e56d93",
     name: "Cardio Area",
   },
   {
-    id: "3",
+    id: "612405ab0df2da8865e56d94",
     name: "Fitness Studio",
   },
   {
-    id: "4",
+    id: "612405ab0df2da8865e56d95",
     name: "Swimming Pool",
   },
   {
-    id: "5",
+    id: "612405ab0df2da8865e56d96",
     name: "Tennis Court",
   },
 ];
 
+const otherFacilities = [
+  {
+    id: "612405ab0df2da8865e56d98",
+    name: "Parking",
+  },
+  {
+    id: "612405ab0df2da8865e56d99",
+    name: "Sauna",
+  },
+  {
+    id: "612405ab0df2da8865e56d9a",
+    name: "Steam Room",
+  },
+  {
+    id: "612405ab0df2da8865e56d9b",
+    name: "Spa",
+  },
+  {
+    id: "612405ab0df2da8865e56d9c",
+    name: "Changing Room",
+  },
+  {
+    id: "612405ab0df2da8865e56d9d",
+    name: "Showers",
+  },
+  {
+    id: "612405ab0df2da8865e56d9e",
+    name: "Restaurant & Bar",
+  },
+  {
+    id: "612405ab0df2da8865e56d9f",
+    name: "Creche",
+  },
+];
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(3),
+  },
+  checkboxesContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "row",
+  },
+  center: {
+    padding: "16px",
+    textAlign: "center",
+  },
+  closeButton: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginBottom: 0,
+  },
+}));
+
 const GymFilter = () => {
+  const isDesktop = useMediaQuery(DESKTOP_BREAKPOINT);
+
+  const classes = useStyles();
+
+  const { handleSubmit, control } = useForm();
+
   const [isOpen, setIsOpen] = React.useState(false);
 
   const toggleDrawer = (open) => (event) => {
@@ -41,11 +114,112 @@ const GymFilter = () => {
     setIsOpen(open);
   };
 
+  const getFacilities = (formData, prefix) => {
+    return Object.entries(formData).reduce((acc, [key, value]) => {
+      if (key.includes(prefix) && value) {
+        return [...acc, key.replace(prefix, "")];
+      }
+      return acc;
+    }, []);
+  };
+
+  const onSubmit = (formData) => {
+    const exerciseFacilities = getFacilities(formData, "exercise_facility_");
+    const otherFacilities = getFacilities(formData, "other_facility_");
+
+    console.log(formData);
+    console.log(exerciseFacilities);
+    console.log(otherFacilities);
+  };
+
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="lg">
       <Button onClick={toggleDrawer(true)}>Filter</Button>
       <Drawer anchor="top" open={isOpen} onClose={toggleDrawer(false)}>
-        <Container maxWidth="sm">All goes here</Container>
+        <Box m={2} className={classes.closeButton}>
+          <CloseIcon onClick={toggleDrawer(false)} />
+        </Box>
+        <Box m={2}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box component="div">
+              <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Exercise Facilities</FormLabel>
+                <FormGroup className={classes.checkboxesContainer}>
+                  {exerciseFacilities.map((exerciseFacility) => {
+                    return (
+                      <FormControlLabel
+                        control={
+                          <Controller
+                            name={`exercise_facility_${exerciseFacility.id}`}
+                            control={control}
+                            defaultValue={false}
+                            render={({ field: { onChange, value } }) => {
+                              return (
+                                <Checkbox checked={value} onChange={onChange} />
+                              );
+                            }}
+                          />
+                        }
+                        label={exerciseFacility.name}
+                        key={exerciseFacility.id}
+                      />
+                    );
+                  })}
+                </FormGroup>
+              </FormControl>
+            </Box>
+            <Divider />
+            <Box component="div">
+              <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Other Facilities</FormLabel>
+                <FormGroup className={classes.checkboxesContainer}>
+                  {otherFacilities.map((otherFacility) => {
+                    return (
+                      <FormControlLabel
+                        control={
+                          <Controller
+                            name={`other_facility_${otherFacility.id}`}
+                            control={control}
+                            defaultValue={false}
+                            render={({ field: { onChange, value } }) => {
+                              return (
+                                <Checkbox checked={value} onChange={onChange} />
+                              );
+                            }}
+                          />
+                        }
+                        label={otherFacility.name}
+                        key={otherFacility.id}
+                      />
+                    );
+                  })}
+                </FormGroup>
+              </FormControl>
+            </Box>
+            <Box component="div" className={classes.center}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disableElevation
+              >
+                Search
+              </Button>
+            </Box>
+            {!isDesktop && (
+              <Box component="div" className={classes.center}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  disableElevation
+                  onClick={toggleDrawer(false)}
+                >
+                  Close
+                </Button>
+              </Box>
+            )}
+          </form>
+        </Box>
       </Drawer>
     </Container>
   );
