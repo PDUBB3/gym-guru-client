dabd9bb0378ddf3c38737ee25e085456d65ce567
Pamela <pamela_bhopal@hotmail.co.uk>
2021-08-26 22:34:57 +0100

    footer added

---
+++ src/components/Footer/index.js
@@ -0,0 +1,30 @@
+// Importing react
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+
+import "./footer.css";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #3a7bd5 30%, #00b4d8 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px #666769",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    alignItems: "center",
+  },
+});
+
+const Footer = () => {
+  const classes = useStyles();
+  return (
+    <div id="footer" className={classes.root}>
+      <Typography> Created 2021</Typography>
+    </div>
+  );
+};
+
+export default Footer;
